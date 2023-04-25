d9883da4c636179f935e924492bbbaa02364b1e3
LucaKSpiller <lukspiller01@gmail.com>
2023-04-25 12:25:53 -0300

    Commit

--- src/main/java/br/com/fiap/game/GamerFeelingApplication.java
+++ src/main/java/br/com/fiap/game/GamerFeelingApplication.java
@@ -1,6 +1,6 @@
 package br.com.fiap.game;
 
-//rm93618 - Luca Katsumata Spiller, RM94250 - MATHEUS GOMES CAMPOLONGO
+//rm93618 - Luca Katsumata Spiller, RM94250 - MATHEUS GOMES CAMPOLONGO-
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
